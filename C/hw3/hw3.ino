90c315939b2404389dd08fec541ca5112b267a96
Milan Wikarski <milan@wikarski.sk>
2020-04-07 22:41:37 +0200

    hw3

--- C/hw3/hw3.ino
+++ C/hw3/hw3.ino
@@ -4,13 +4,13 @@
 #define N_LEDS 4
 #define N_BTNS 3
 
-#define PRESS_DELAY 1000
-#define PRESS_PERIOD 300
+#define PRESS_DELAY 700
+#define PRESS_PERIOD 250
 
 const int led_pins[] = {10, 11, 12, 13};
 const int btn_pins[] = {A1, A2, A3};
 
-const int btn_actions[] = {15, 1, 3};
+const int btn_actions[] = {1, 15, 3};
 
 unsigned short count;
 unsigned long t;
@@ -19,15 +19,16 @@ short btn_pressed[N_BTNS];
 unsigned long btn_emmit_at[N_BTNS];
 unsigned long btn_last_interaction[N_BTNS];
 
-
-void led(unsigned short pins) {
+void led(unsigned short pins)
+{
   pins = pins & 0xF;
-  
+
   for (int i = 0; i < N_LEDS; i++)
     digitalWrite(led_pins[i], ((pins & (0x1 << i)) > 0) ? LOW : HIGH);
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
   count = 0;
@@ -35,46 +36,53 @@ void setup() {
   for (int i = 0; i < N_LEDS; i++)
     pinMode(led_pins[i], OUTPUT);
 
-  for (int i = 0; i < N_BTNS; i++) {
+  for (int i = 0; i < N_BTNS; i++)
+  {
     pinMode(btn_pins[i], INPUT);
-    
+
     btn_pressed[i] = 0;
     btn_emmit_at[i] = 0;
     btn_last_interaction[i] = 0;
-  }  
+  }
 }
 
-void loop() {
+void loop()
+{
   t = millis();
 
   led(count);
 
-  for (int i = 0; i < N_BTNS; i++) {
+  for (int i = 0; i < N_BTNS; i++)
+  {
     short pressed = abs(digitalRead(btn_pins[i]) - 1);
     short delta = pressed - btn_pressed[i];
 
     // If the button pressed or released
-    if (delta != 0) {
+    if (delta != 0)
+    {
       // press and realease at the exact same time fallback
       if (btn_last_interaction[i] == t)
         continue;
-      
+
       btn_last_interaction[i] = t;
-      
+
       Serial.print("Button ");
       Serial.print(i);
       Serial.println(delta == 1 ? " pressed" : " released");
     }
 
     // If button pressed
-    if (delta == 1) {
+    if (delta == 1)
+    {
       count = (count + btn_actions[i]) % N_PERIODS;
       btn_emmit_at[i] = t + PRESS_DELAY; // Plan next tick
-    } 
+    }
 
     // If button down
-    if (delta == 0 && pressed == 1) {
-      if (t >= btn_emmit_at[i]) {
+    if (delta == 0 && pressed == 1)
+    {
+      if (t >= btn_emmit_at[i])
+      {
         count = (count + btn_actions[i]) % N_PERIODS;
         btn_emmit_at[i] += PRESS_PERIOD; // Plan next tick
 
@@ -83,8 +91,7 @@ void loop() {
         Serial.println(" down");
       }
     }
-    
+
     btn_pressed[i] = pressed;
   }
 }
-
